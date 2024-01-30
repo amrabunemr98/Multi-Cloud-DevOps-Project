1746d78073232aaedefc0891398b26b4cf41e0a1
Amr Abu-Nemr <128842547+amrabunemr98@users.noreply.github.com>
2024-01-30 20:48:58 +0100

    Update BuildUnittest.groovy

--- vars/BuildUnittest.groovy
+++ vars/BuildUnittest.groovy
@@ -1,5 +1,5 @@
 def BuildAppAndUnitTest(dockerfileapp = 'Build-UntitTest') {
     script {
-    sh "docker build -t gradle-test ${docker_file_app} ."
+    sh "docker build -t gradle-test ${dockerfileapp} ."
     }
 }
