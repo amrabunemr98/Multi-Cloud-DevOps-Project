2399669a5b66a0cbd8f7edfbe3d3ee4a59502c9e
Amr Abu-Nemr <128842547+amrabunemr98@users.noreply.github.com>
2024-01-30 22:55:49 +0100

    Update SonarQubeTest.groovy

--- vars/SonarQubeTest.groovy
+++ vars/SonarQubeTest.groovy
@@ -1,4 +1,4 @@
-def call(Token_Sonar, SONAR_SCANNER_HOME, SONAR_PROJECT_KEY, SONAR_HOST_URL) {
+def lll(Token_Sonar, SONAR_SCANNER_HOME, SONAR_PROJECT_KEY, SONAR_HOST_URL) {
     script{
         withCredentials([string(credentialsId: 'Token_Sonar', variable: 'SONAR_TOKEN')]) {
         // Run SonarQube analysis with the token
