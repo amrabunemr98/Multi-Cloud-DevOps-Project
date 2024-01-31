a34ff882e8ba10d6e82cf0d9693e259b10d5b0c7
Amr Abu-Nemr <128842547+amrabunemr98@users.noreply.github.com>
2024-01-31 08:31:31 +0100

    Update SonarQube.groovy

--- vars/SonarQube.groovy
+++ vars/SonarQube.groovy
@@ -1,9 +1,18 @@
 def call(Token_Sonar, SONAR_PROJECT_KEY, SONAR_HOST_URL) {
-    script{
+    script {
         withCredentials([string(credentialsId: 'Token_Sonar', variable: 'SONAR_TOKEN')]) {
-        // Run SonarQube analysis with the token 
-        def scannerHome = tool 'SonarQube'
-        sh "${scannerHome}/bin/sonar-scanner -X -Dsonar.projectKey=${SONAR_PROJECT_KEY} -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_TOKEN} -Dsonar.scm.provider=git -Dsonar.java.binaries=${scannerHome}/build/classes/java/main"
+            // Run SonarQube analysis with the token 
+            def scannerHome = tool 'SonarQube'
+            def classesDir = "${env.WORKSPACE}/build/classes/java/main"  // Adjust this path based on my project structure
+            sh """
+                ${scannerHome}/bin/sonar-scanner \
+                -X \
+                -Dsonar.projectKey=${SONAR_PROJECT_KEY} \
+                -Dsonar.host.url=${SONAR_HOST_URL} \
+                -Dsonar.login=${SONAR_TOKEN} \
+                -Dsonar.scm.provider=git \
+                -Dsonar.java.binaries=${classesDir}
+            """
         }
     }
 }
