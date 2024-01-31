996a3058fba59a61ed74671221b72f396859ccf9
Amr Abu-Nemr <128842547+amrabunemr98@users.noreply.github.com>
2024-01-31 09:38:31 +0100

    Update SonarQube.groovy

--- vars/SonarQube.groovy
+++ vars/SonarQube.groovy
@@ -1,8 +1,8 @@
 def call(Token_Sonar, SONAR_PROJECT_KEY, SONAR_HOST_URL) {
     script {
         withCredentials([string(credentialsId: 'Token_Sonar', variable: 'SONAR_TOKEN')]) {
-            // Run SonarQube analysis with the token 
-            def scannerHome = tool 'SonarQube'
+            // This assumes that the SonarQube Scanner tool is configured in the Jenkins Global Tool Configuration.
+            def scannerHome = tool 'SonarQube'   // Retrieve the installation path of the SonarQube Scanner tool defined in Jenkins.
             def classesDir = "${env.WORKSPACE}/build/classes/java/main"  // Adjust this path based on my project structure
             sh """
                 ${scannerHome}/bin/sonar-scanner \
